7cb6d25f7d51e3533225f77cc0ef244c0d0722cc
Eyu Kassa <eeyyuuk@gmail.com>
2025-07-12 19:26:48 +0000

    update

--- cinema_service/cinema_project/cinema/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Cinema, Auditorium, Seat
-
-# Register your models here.
-admin.site.register(Cinema)
-admin.site.register(Auditorium)
-admin.site.register(Seat)
